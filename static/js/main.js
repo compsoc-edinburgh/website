73c9b7634f04880382b1286c388678ca627c193e
JMaio <j.f.maio98@gmail.com>
2018-04-15 20:36:04 +0200

    add call to sly

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+jQuery(function($){
+  'use strict';
+  // -------------------------------------------------------------
+	//   Centered Navigation
+	// -------------------------------------------------------------
+	(function () {
+		var $frame = $('#front-sigs');
+		var $wrap  = $frame.parent();
+
+		// Call Sly on frame
+		$frame.sly({
+			horizontal: 1,
+			itemNav: 'centered',
+			smart: 1,
+			activateOn: 'click',
+			mouseDragging: 1,
+			touchDragging: 1,
+			releaseSwing: 1,
+			startAt: 4,
+			scrollBar: $wrap.find('.scrollbar'),
+			scrollBy: 1,
+			speed: 300,
+			elasticBounds: 1,
+			easing: 'easeOutExpo',
+			dragHandle: 1,
+			dynamicHandle: 1,
+			clickBar: 1,
+
+			// Buttons
+			prev: $wrap.find('.prev'),
+			next: $wrap.find('.next')
+		});
+	}());
+});
